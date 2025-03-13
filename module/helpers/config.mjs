a2694d1c92865ae7d7a5bf75cacdb2ab9cd40f02
setuna00 <jingx.aus@gmail.com>
2025-03-13 23:20:36 +1100

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const FGOVTT = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+FGOVTT.abilities = {
+  str: 'FGOVTT.Ability.Str.long',
+  dex: 'FGOVTT.Ability.Dex.long',
+  con: 'FGOVTT.Ability.Con.long',
+  int: 'FGOVTT.Ability.Int.long',
+  wis: 'FGOVTT.Ability.Wis.long',
+  cha: 'FGOVTT.Ability.Cha.long',
+};
+
+FGOVTT.abilityAbbreviations = {
+  str: 'FGOVTT.Ability.Str.abbr',
+  dex: 'FGOVTT.Ability.Dex.abbr',
+  con: 'FGOVTT.Ability.Con.abbr',
+  int: 'FGOVTT.Ability.Int.abbr',
+  wis: 'FGOVTT.Ability.Wis.abbr',
+  cha: 'FGOVTT.Ability.Cha.abbr',
+};
